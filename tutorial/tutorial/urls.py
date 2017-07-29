55a4edb6467eff4e37cbf4dda5a3cbd1439a90c6
liupai <1565730756@qq.com>
2017-07-29 13:37:32 +0800

    添加admin管理界面

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url, include
 
+
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^', include('snippets.urls')),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
